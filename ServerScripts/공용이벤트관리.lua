ba40c74d06dd2216ee04b8765d9a390de3f33775
kimhyeonwoo <qkenrkhw5510@gmail.com>
2025-07-18 15:48:38 +0900

    공용이벤트 - 플레이어 이동 (상,하,좌,우) 추가 버튼 클릭시 상하좌우 이동

---
+++ ServerScripts/공용이벤트관리.lua
@@ -0,0 +1,15 @@
+Server.GetTopic("공용000").Add(function(text)
+unit.StartGlobalEvent(000)
+end)
+
+Server.GetTopic("공용001").Add(function(text)
+unit.StartGlobalEvent(001)
+end)
+
+Server.GetTopic("공용002").Add(function(text)
+unit.StartGlobalEvent(002)
+end)
+
+Server.GetTopic("공용003").Add(function(text)
+unit.StartGlobalEvent(003)
+end)
